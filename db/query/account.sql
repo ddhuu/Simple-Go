d77f65aa6f370ab0f1284d743a5ef7b4b392daf7
ddhuu <ddhuu.dev@gmail.com>
2024-09-23 22:51:40 +0700

    feat: add CI

--- db/query/account.sql
+++ db/query/account.sql
@@ -30,7 +30,7 @@ DELETE FROM accounts WHERE id = $1;
 
 -- name: GetAccountForUpdate :one
 SELECT * FROM accounts
-WHERE id = $1 LIMIT 1 FOR UPDATE;
+WHERE id = $1 LIMIT 1 FOR NO KEY  UPDATE;
 
 -- name: AddAccountBalance :one
 UPDATE accounts
